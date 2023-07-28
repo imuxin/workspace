073820c904bf0d08d31ff1ef57af58edfc216c54
chengqinglin <chengqinglin@icloud.com>
2023-07-28 20:34:23 +0800

    add NvChad custom config
    Signed-off-by: chengqinglin <chengqinglin@icloud.com>

---
+++ NvChad-Config/custom/configs/overrides.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+M.treesitter = {
+    ensure_installed = {
+      "c",
+      "lua",
+      "go",
+      "rust",
+      "yaml",
+      "json",
+      "toml",
+    },
+    indent = {
+      enable = true,
+      -- disable = {
+      --   "python"
+      -- },
+    },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    -- "typescript-language-server",
+    -- "deno",
+    -- "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+
+    -- go
+    "gopls",
+
+    -- rust
+    "rust-analyzer",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
